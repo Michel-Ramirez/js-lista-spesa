f4089fcf745107ef681e26698a092cecb317f514
Michel-Ramirez <michel.ramirez19@gmail.com>
2023-06-05 15:01:49 +0200

    btn to add item

--- js/script.js
+++ js/script.js
@@ -2,42 +2,51 @@ console.log('JS ok')
 
 // prendo l'elemento dal dom
 const shopingList = document.getElementById('shoping-list');
+const addItems = document.getElementById('item');
+const btn = document.getElementById('btn-add');
+
 
 // creo lista della spesa con un array
-const listItem = ['pane', 'acqua', 'biscotti', 'latte', 'caffè', 'zucchero', 'farina'];
+let listItem = [];
 
 
+btn.addEventListener('click', function(){
 
-//Monto la lista
-let list = `<ul class="list-group">`;
-
-/*****************\| CICLO CON FOR |/ *****************/
-
-// for (i = 0; i < listItem.length; i++) {
-//     list += `<li class="list-group-item">${listItem[i]}</li>`;
-// }
-
-
-/*****************\| CICLO CON WHILE |/ *****************/
-
-//scompongo il ciclo del for utilizzando while
-
-// counter
-let i = 0;
-// condizione
-while (i < listItem.length) {
-    list += `<li class="list-group-item">${listItem[i]}</li>`;
-    // incromento
-    i++;
-}
-
-
-//Chiudo la lista
-list += `</ul>`
- 
-//Stampo nel dom
-shopingList.innerHTML = list
+    //aggiungo la lista le stringhe inserite dal utente
+   listItem.push(addItems.value.trim())
     
     
+    //Monto la lista
+    let list = `<ul class="list-group">`;
+    
+    /*****************\| CICLO CON FOR |/ *****************/
+    
+    // for (i = 0; i < listItem.length; i++) {
+    //     list += `<li class="list-group-item">${listItem[i]}</li>`;
+    // }
+    
+    
+    /*****************\| CICLO CON WHILE |/ *****************/
+    
+    //scompongo il ciclo del for utilizzando while
+    
+    // counter
+    let i = 0;
+    // condizione
+    while (i < listItem.length) {
+        list += `<li class="list-group-item">${listItem[i]}</li>`;
+        // incromento
+        i++;
+    }
+    
+    
+    //Chiudo la lista
+    list += `</ul>`
+     
+    //Stampo nel dom
+    shopingList.innerHTML = list
+        
+        
+});
     
 
